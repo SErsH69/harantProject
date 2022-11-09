46ea815a1ed8b3b4c4485c041bfd712c469ccc96
Гончаров Сергей <Goncharov-5769@main-fer.fnpz.uz>
2022-11-09 17:54:48 +0500

    правки

--- js/main.js
+++ js/main.js
@@ -2,5 +2,14 @@ $(function() {
     $('.faq_block__tit').on('click', function() {
         $(this).toggleClass('isOpened');
         $(this).parent().toggleClass('isOpened');
-    })
+    });
+    $('.cards_block__pref--more').on('click', function() {
+        $(this).parent().find('.cards_block__pref').toggleClass('isActive');
+        $(this).toggleClass('isOpened');
+        if($(this).hasClass('isOpened')) {
+            $(this).text('Скрыть')
+        } else {
+            $(this).text('Развернуть')
+        }
+    });
 })
